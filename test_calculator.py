ed00488f00733322ae6bc99b7bcb33ae049e5127
Patrick <pliao28@gmail.com>
2020-05-03 16:17:29 -0400

    Division function and test

--- test_calculator.py
+++ test_calculator.py
@@ -16,3 +16,6 @@ class TestCalculator:
 
     def test_multiplaction(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 100 == calculator.multiply(10, 10)
